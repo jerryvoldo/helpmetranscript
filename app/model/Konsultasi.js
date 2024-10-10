6bb07364d656a1585ca2037dcf3139e7dc01b751
Jerry Manurung <jerry.voldo@gmail.com>
2024-10-10 10:39:42 +0000

    Oct 10, 2024, 5:39 PM

--- app/model/Konsultasi.js
+++ app/model/Konsultasi.js
@@ -1,2 +1,65 @@
 import mongoose from "mongoose";
 
+const CatatanKhusus = mongoose.Schema({
+    tanggal: Date,
+    isi: String,
+    issued_by: String,
+    status: String
+});
+
+const Konsultan = mongoose.Schema({
+    nama: String,
+    nip: String,
+    divisi: String,
+    path_ttd_file: String,
+});
+
+const Hasil = mongoose.Schema({
+    kategori: String,
+    detail: String,
+    tindaklanjut: String
+});
+
+const TranskripKonsultasi = mongoose.Schema({
+    nomor: String,
+    datetime_kirim: Date,
+    status_kirim: String,
+    path_file: String
+})
+
+const CatatanKonsultasi = mongoose.Schema({
+    datetime_mulai: Date,
+    datetime_selesai: Date,
+    loket: String,
+    topik: String,
+    konsultan: Konsultan,
+    hasil: Hasil,
+    status: String,
+    transkrip: [TranskripKonsultasi]
+})
+
+const AsosiasiPerusahaan = mongoose.Schema(
+    {
+        nama: String,
+        alamat: String,
+        jenis: String,
+        notlpn: String,
+        nowa: String,
+        email: String,
+        pimpinan: String,
+        catatan_khusus: [CatatanKhusus],
+    }
+)
+
+const Konsultasi = mongoose.Schema(
+    {
+        nama_client: String,
+        alamat: String,
+        notlpn: String,
+        nowa: String,
+        email: String,
+        asosiasi_perusahaan: [AsosiasiPerusahaan],
+        konsultasi: [CatatanKonsultasi],
+        path_ttd_file: String,
+    }
+)
