23edf3f3be33cf43f566f2d2e4ad858fa41f6e9a
Jerry Manurung <jerry.voldo@gmail.com>
2024-10-10 07:34:47 +0000

    Oct 10, 2024, 2:34 PM

--- app/api/route.js
+++ app/api/route.js
@@ -3,17 +3,5 @@ import connectDB from "../lib/connectdb";
 
 export async function POST(request)
 {
-    await connectDB();
-
-    const {name, age} = request.body;
-
-    console.log(JSON.parse(request.body));
-
-    const person = new user({
-        name:name,
-        age:age
-    })
-
-    await person.save();
-    console.log("inside api ", name, age);
+    console.log(request);
 }
