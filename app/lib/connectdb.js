19f31d3e089e8f33ae52043644799fef05f65ccc
Jerry Manurung <jerry.voldo@gmail.com>
2024-10-10 08:23:21 +0000

    Oct 10, 2024, 3:23 PM

--- app/lib/connectdb.js
+++ app/lib/connectdb.js
@@ -20,6 +20,7 @@ async function connectDB() {
     if (!cached.promise) {
         const opts = {
           bufferCommands: false,
+          dbName: "transcripts",
         };
 
         cached.promise = mongoose.connect(DATABASE_URL, opts).then((mongoose) => {
