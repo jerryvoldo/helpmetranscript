8de8ede31a0e017c2dfa14653afb58fafe1e8a35
Jerry Manurung <jerry.voldo@gmail.com>
2024-10-10 07:42:48 +0000

    Oct 10, 2024, 2:42 PM

--- app/api/route.js
+++ app/api/route.js
@@ -3,5 +3,8 @@ import connectDB from "../lib/connectdb";
 
 export async function POST(request)
 {
-    console.log(request);
+    const res = await request.json();
+    console.log(res);
+
+    return Response.json({ res })
 }
