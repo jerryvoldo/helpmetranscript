4b1a0841732efbeed874e7d87d42ce75f36a9095
Jerry Manurung <jerry.voldo@gmail.com>
2024-10-10 06:18:53 +0000

    Oct 10, 2024, 1:18 PM

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
-      test baru
+      <form action="./api/data" method="POST">
+        <label>Enter name</label>
+        <input className="border" type="text" name="name" id="name"></input>
+        
+        <label>Enter age</label>
+        <input className="border" type="number" name="age" id="age"></input>
+
+        <button className="border rounded px-1" type="submit">Submit</button>
+      </form>
     </div>
   );
 }
