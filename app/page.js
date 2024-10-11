63a0b697261adc3084d7bc79a8e517378815ed8e
Jerry Manurung <jerry.voldo@gmail.com>
2024-10-11 23:02:02 +0000

    Oct 12, 2024, 6:02 AM

--- app/page.js
+++ app/page.js
@@ -27,6 +27,25 @@ export default function Home() {
         </label>
 
         <p className="h3 my-4">Perusahaan</p>
+        <label className="mb-2 input input-bordered flex items-center gap-2">Nama
+          <input className="grow" type="text" name="nama_perusahaan" id="nama_perusahaan" />
+        </label>
+        
+        <label className="mb-2 input input-bordered flex items-center gap-2">Alamat
+          <input className="grow" type="text" name="alamat_perusahaan" id="alamat_perusahaan" />
+        </label>
+
+        <label className="mb-2 input input-bordered flex items-center gap-2">Nomor Telepon
+          <input className="grow" type="text" name="notlpn_perusahaan" id="notlpn_perusahaan" />
+        </label>
+
+        <label className="mb-2 input input-bordered flex items-center gap-2">Nomor Whatsapp
+          <input className="grow" type="text" name="nowa_perusahaan" id="nowa_perusahaan" />
+        </label>
+
+        <label className="mb-2 input input-bordered flex items-center gap-2">Email
+          <input className="grow" type="email" name="email_perusahaan" id="email_perusahaan" />
+        </label>
 
         <button className="btn btn-primary" type="submit">Submit</button>
       </form>
