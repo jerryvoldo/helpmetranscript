df65582b080c4f30499a43466b204de8db016973
Jerry Manurung <jerry.voldo@gmail.com>
2024-10-11 22:11:10 +0000

    Oct 12, 2024, 5:11 AM

--- app/page.js
+++ app/page.js
@@ -3,11 +3,13 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div>
+      <label className="h4">Nama Pengkonsul</label>
+      
       <form action="/api" method="POST">
-        <label>Enter name</label>
+        <label>Nama</label>
         <input className="border" type="text" name="name" id="name"></input>
         
-        <label>Enter age</label>
+        <label>ALamat</label>
         <input className="border" type="number" name="age" id="age"></input>
 
         <button className="border rounded px-1" type="submit">Submit</button>
