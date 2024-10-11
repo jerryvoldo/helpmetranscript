84c7c5ff476d1a58156c03d82f49540321127199
Jerry Manurung <jerry.voldo@gmail.com>
2024-10-11 22:15:48 +0000

    Oct 12, 2024, 5:15 AM

--- app/page.js
+++ app/page.js
@@ -4,15 +4,15 @@ export default function Home() {
   return (
     <div>
       <label className="h4">Nama Pengkonsul</label>
-      
+
       <form action="/api" method="POST">
         <label>Nama</label>
-        <input className="border" type="text" name="name" id="name"></input>
+        <input className="input input-bordered w-full max-w-xs" type="text" name="name" id="name"></input>
         
         <label>ALamat</label>
-        <input className="border" type="number" name="age" id="age"></input>
+        <input className="input input-bordered w-full max-w-xs" type="number" name="age" id="age"></input>
 
-        <button className="border rounded px-1" type="submit">Submit</button>
+        <button className="btn btn-primary" type="submit">Submit</button>
       </form>
     </div>
   );
