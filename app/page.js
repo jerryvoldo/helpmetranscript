69069885b11fed415f0c04e6df4472fdff00008e
Jerry Manurung <jerry.voldo@gmail.com>
2024-10-12 05:58:16 +0000

    Oct 12, 2024, 12:58 PM

--- app/page.js
+++ app/page.js
@@ -3,10 +3,13 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div>
+      <h2>Input email untuk memulai</h2>
       <form action="/api" method="POST">
-            <label className="mb-2 input input-bordered flex items-center gap-2">Nama
-                <input className="grow" type="text" name="nama" id="nama" />
+            <label className="mb-2 input input-bordered flex items-center gap-2">Email
+                <input className="grow" type="email" name="email" id="email" />
             </label>
+
+            <button type="submit" className="btn btn-primary">Selanjutnya</button>
       </form>
     </div>
   );
