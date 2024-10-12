ee9433053737c4eeea8a817dd33f4dfd3b6093d6
Jerry Manurung <jerry.voldo@gmail.com>
2024-10-12 06:02:08 +0000

    Oct 12, 2024, 1:02 PM

--- app/api/route.js
+++ app/api/route.js
@@ -4,39 +4,14 @@ import connectDB from "../lib/connectdb";
 export async function POST(request)
 {
     const formData = await request.formData();
-    const nama = formData.get('nama');
-    const alamat = formData.get('alamat');
-    const notlpn = formData.get('notlpn');
-    const nowa = formData.get('nowa');
+    
     const email = formData.get('email');
 
-    const nama_perusahaan = formData.get('nama_perusahaan');
-    const alamat_perusahaan = formData.get('alamat_perusahaan');
-    const notlpn_perusahaan = formData.get('notlpn_perusahaan');
-    const nowa_perusahaan = formData.get('nowa_perusahaan');
-    const email_perusahaan = formData.get('email_perusahaan');
-
-    const objClient = {
-        nama:nama,
-        alamat:alamat,
-        notlpn:notlpn,
-        nowa:nowa,
-        email:email,
-        asosiasi_perusahaan : 
-        {
-            nama:nama_perusahaan,
-            alamat:alamat_perusahaan,
-            notlpn:notlpn_perusahaan,
-            nowa:nowa_perusahaan,
-            email:email_perusahaan,
-        }
-    };
-    
     await connectDB();
 
-    const consult = new client(objClient)
+    const consult = new client();
 
-    await consult.save();
+    const data = await consult.find({nama:email}).exec();
 
-    return Response.json(objClient);
+    return Response.json(data);
 }
